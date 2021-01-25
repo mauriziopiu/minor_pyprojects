9c5392d2c69c91a4987787779e0ba1f950e9181f
mauriziopiu <maurizio.piu427@gmail.com>
2021-01-25 20:08:43 +0100

    GT: implemented feature: multiple edges between different nodes, started implementation of directed graphs

--- Graph_Theory/Graph.py
+++ Graph_Theory/Graph.py
@@ -1,14 +1,13 @@
 import pygame
+import math
 
 
 class Graph(object):
 
-    def __init__(self):
+    def __init__(self, isDirected=False):
+        self.isDirected = isDirected
         self.nodes = []
         self.edges = []
-
-    def updateGraph(self):
-        pass
 
     def addNode(self, win, x, y):
         new_node = Node(x, y)
@@ -32,7 +31,15 @@ class Graph(object):
     def addEdge(self, win, start_node, end_node, weight=1):
         new_edge = Edge(start_node, end_node)
         self.edges.append(new_edge)
+        start_node.addEdge(new_edge)
+        end_node.addEdge(new_edge)
         new_edge.draw(win)
+
+    def isEdgeAt(self, x, y):
+        pass
+
+    def getEdgeAt(self, x, y):
+        pass
 
 
 class Node(object):
@@ -50,6 +57,9 @@ class Node(object):
         self.radius = radius
         self.edges = []
 
+    def addEdge(self, edge):
+        self.edges.append(edge)
+
     def draw(self, win):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
@@ -58,13 +68,33 @@ class Edge(object):
 
     def __init__(self, start_node, end_node, color=(0, 0, 0), width=3, weight=1):
         self.start_node = start_node
+        self.start_node.x = start_node.x
+        self.start_node.y = start_node.y
+
         self.end_node = end_node
+        self.end_node_x = end_node.x
+        self.end_node_y = end_node.y
+
+        self.delta_x = self.end_node.x - self.start_node.x
+        self.delta_y = self.end_node.y - self.start_node.y
+        self.hyp = math.sqrt(self.delta_x**2 + self.delta_y**2)
+        self.normalized_dir_vx = self.delta_x // self.hyp
+        self.normalized_dir_vy = self.delta_y // self.hyp
+        self.direction = math.acos(self.delta_y // self.hyp)        # not sure
+
         self.color = color
         self.width = width
         self.weight = weight
 
     def draw(self, win):
-        pygame.draw.line(win, self.color, (self.start_node.x, self.start_node.y), (self.end_node.x, self.end_node.y), self.width)
+        pygame.draw.line(win, self.color, (self.start_node.x, self.start_node.y), (self.end_node.x, self.end_node.y),
+                         self.width)
+
+    def drawDirection(self, win):
+        tip_x = self.end_node.x + math.sin(self.end_node.radius)
+        tip_y = self.end_node.y - math.cos(self.end_node.radius)
+        tip = tip_x, tip_y
+        pygame.draw.polygon()
 
 
 WHITE = (255, 255, 255)
@@ -81,7 +111,7 @@ def main():
     run = True
 
     graph = Graph()
-    edgePosition1 = []
+    nodeStorage = []
 
     while run:
         clock.tick(27)
@@ -95,14 +125,15 @@ def main():
                 pos = pygame.mouse.get_pos()
 
                 if graph.isNodeAt(pos[0], pos[1]) and len(graph.nodes) >= 2:  # Click on Node (-> Add Edge)
-                    if len(edgePosition1) == 0:  # No node chosen
-                        edgePosition1.append((pos[0], pos[1]))
-                    elif len(edgePosition1) == 1:           # First node chosen
-                        start_node = graph.getNodeAt(edgePosition1[0][0], edgePosition1[0][1])
+                    if len(nodeStorage) == 0:  # No node chosen
+                        nodeStorage.append((pos[0], pos[1]))
+                    elif len(nodeStorage) == 1:  # First node chosen
+                        start_node = graph.getNodeAt(nodeStorage[0][0], nodeStorage[0][1])
                         end_node = graph.getNodeAt(pos[0], pos[1])
                         graph.addEdge(win, start_node, end_node)
-                # elif False:                             # Click on Edge (-> Edit Edge)
-                #   pass
+                        nodeStorage.clear()
+                if graph.isEdgeAt(pos[0], pos[1]):
+                    pass
                 else:  # Click into space (-> Add Node)
                     graph.addNode(win, pos[0], pos[1])
 
