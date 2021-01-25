3a91bf85f93f33875dc21eca1b90185deed74c03
mauriziopiu <maurizio.piu427@gmail.com>
2021-01-25 20:27:54 +0100

    deleted empty files edge and node

--- Graph_Theory/Edge.py
+++
@@ -1,8 +0,0 @@
-
-class Edge(object):
-
-    def __init__(self, start_node, end_node, weight=1):
-        self.start_node = start_node
-        self.end_node = end_node
-        self.weight = weight
-
